76bfe652aa7e80269f96a7fac0d603c80bae78f4
Gabriel Rumbaut <grumbaut@gmail.com>
2018-05-26 15:37:07 -0400

    added sign-in form

---
+++ components/sessions/SignIn.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Text, View, TextInput } from 'react-native';
+import { FormLabel, FormInput, FormValidation } from 'react-native-elements';
+import { connect } from 'react-redux';
+import { getOrganizationsFromServer } from '../store';
+
+class SignIn extends React.Component {
+  render() {
+    return (
+      <View>
+
+      </View>
+    )
+  }
+}
