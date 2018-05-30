2e958bac71d6285a406807d572e47f7275c7a0ef
Jeremy Grubard <jgrubard@gmail.com>
2018-05-30 18:52:44 -0400

    request pending once sent

--- components/OrganizationInfo.js
+++ components/OrganizationInfo.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Text, View, StyleSheet, ScrollView } from 'react-native';
-import { Button } from 'react-native-elements';
+import { View, StyleSheet, ScrollView, Image } from 'react-native';
+import { Button, Text } from 'react-native-elements';
 import { createOrganizationRequestOnServer } from '../store';
 
 class OrganizationInfo extends React.Component {
@@ -12,50 +12,78 @@ class OrganizationInfo extends React.Component {
   }
 
   render() {
-    const { user } = this.props;
-    const organization = this.props.navigation.getParam('organization', 'no organization');
+    const { user, organization, ownRequest } = this.props;
     return (
       <ScrollView>
+      <View style={styles.image}>
+        { organization.image && <Image
+          style={{ width: 400, height: 275 }}
+          source={{ uri: organization.image }}
+        />}
+      </View>
       <View style={styles.container}>
-        <Text style={{ fontSize: 35, textAlign: 'left' }}>
+        <Text h3 style={styles.text}>
           {organization.name}
         </Text>
-        <Text style={{ fontSize: 25, textAlign: 'left', marginBottom: 20 }}>
+        <Text style={[ styles.text, { marginBottom: 20 } ]}>
           ({organization.organization_type})
         </Text>
-        <Text style={{ fontSize: 30, textAlign: 'left' }}>
-          {organization.address}
-        </Text>
-        <Text style={{ fontSize: 22, textAlign: 'left' }}>
-          {organization.city}, {organization.state} {organization.zip}
-        </Text>
-        <Text style={{ fontSize: 22, textAlign: 'left' }}>
+        <Text style={[ styles.text, { fontSize: 20 } ]}>
           {organization.contact_phone}
         </Text>
-        <Button
-          raised
-          buttonStyle={{ backgroundColor: 'green', borderRadius: 10, marginTop: 15 }}
-          title='Check In'
-          onPress={() => console.log('this will check in a user')}
-        />
-        <Button
-          raised
-          buttonStyle={{ backgroundColor: 'skyblue', borderRadius: 10, marginTop: 15 }}
-          title='Request to Join'
-          onPress={() => this.props.createOrganizationRequest({ userId: user.id, organizationId: organization.id })}
-        />
+        <Text style={styles.text}>
+          {organization.address}
+        </Text>
+        <Text style={styles.text}>
+          {organization.city}, {organization.state} {organization.zip}
+        </Text>
+        {
+          !ownRequest ? (
+            <Button
+              raised
+              buttonStyle={{ backgroundColor: 'skyblue', borderRadius: 10, marginTop: 15 }}
+              title='Request to Join'
+              onPress={() => {
+                // console.log({ userId: user.id, organizationId: organization.id })
+                this.props.createOrganizationRequest({ userId: user.id, organizationId: organization.id })
+              }}
+            />
+          ) : (
+            ownRequest.status === 'pending' ? (
+            <Button
+              raised
+              buttonStyle={{ backgroundColor: 'green', borderRadius: 10, marginTop: 15 }}
+              title='Request Pending'
+              onPress={() => console.log('this will check in a user')}
+              disabled={true}
+            />
+            ) : (
+              <Button
+                raised
+                buttonStyle={{ backgroundColor: 'green', borderRadius: 10, marginTop: 15 }}
+                title='Check In'
+                onPress={() => console.log('this will check in a user')}
+              />
+            )
+
+          )
+        }
       </View>
       </ScrollView>
     );
   }
 }
 
-const mapState = (state) => {
-  const organizationRequests = state.organizationRequests
-  const user = state.user
-  // console.log(state)
+const mapState = ({ organizationRequests, user }, { navigation }) => {
+  const organization = navigation.getParam('organization', 'no organization');
+  const ownRequest = organizationRequests.find(request => {
+    return request.userId === user.id && request.organizationId === organization.id
+  })
+  console.log(ownRequest)
   return {
     user,
+    ownRequest,
+    organization,
     organizationRequests
   }
 }
@@ -73,6 +101,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20
+  },
+  image: {
+    flex: 1
+  },
+  text: {
+    textAlign: 'left'
   }
 });
 
