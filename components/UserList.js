57d425446322e75994301f43eebb764ae6d5f16f
Jeremy Grubard <jgrubard@gmail.com>
2018-06-04 13:39:52 -0400

    user request button disabled if request already made

--- components/UserList.js
+++ components/UserList.js
@@ -7,43 +7,47 @@ import { createUserRequestOnServer } from '../store';
 class UserList extends Component {
 
   render() {
-    const { loggedUser, ownUsers, createRequest } = this.props;
+    const { loggedUser, ownUsers, createRequest, userRequests } = this.props;
     return (
       <View>
       <Text h4 style={{ textAlign: 'center', marginTop: 20 }}>Pair Up!</Text>
         {
-          ownUsers.map(user => (
-            <View
-              key={user.id}
-              style={{
-                borderColor: 'grey',
-                borderWidth: 1,
-                borderRadius: 10,
-                padding: 20,
-                margin: 10
-              }}>
-              <Text style={{ textAlign: 'center', fontSize: 18 }}>{user.fullName}</Text>
-              {
-                <Button
-                  onPress={() => createRequest({ requesterId: loggedUser.id, responderId: user.id })}
-                  title='Send Request'
-                  buttonStyle={{
-                    backgroundColor: 'blue',
-                    borderRadius: 10,
-                    marginTop: 15
-                  }}
-                />
-              }
+          ownUsers.map(user => {
+            const requestSent = !!userRequests.find(request => request.requesterId === loggedUser.id && request.responderId === user.id)
+            return (
+              <View
+                key={user.id}
+                style={{
+                  borderColor: 'grey',
+                  borderWidth: 1,
+                  borderRadius: 10,
+                  padding: 20,
+                  margin: 10
+                }}>
+                <Text style={{ textAlign: 'center', fontSize: 18 }}>{user.fullName}</Text>
+                {
+                  <Button
+                    onPress={() => createRequest({ requesterId: loggedUser.id, responderId: user.id })}
+                    title='Send Request'
+                    buttonStyle={{
+                      backgroundColor: 'blue',
+                      borderRadius: 10,
+                      marginTop: 15
+                    }}
+                    disabled={requestSent}
+                  />
+                }
 
-            </View>
-          ))
+              </View>
+            )
+          })
         }
       </View>
     );
   }
 }
 
-const mapState = ({ user, users, userOrganizations }, { organization }) => {
+const mapState = ({ user, users, userOrganizations, userRequests }, { organization }) => {
   const ownUsers = userOrganizations.reduce((memo, userOrg) => {
     users.forEach(otherUser => {
       if (userOrg.userId === otherUser.id && userOrg.organizationId === organization.id && user.id !== otherUser.id) {
@@ -55,7 +59,8 @@ const mapState = ({ user, users, userOrganizations }, { organization }) => {
   const loggedUser = user;
   return {
     ownUsers,
-    loggedUser
+    loggedUser,
+    userRequests
   }
 }
 
