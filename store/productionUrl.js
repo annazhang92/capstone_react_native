650b70a58b75d08061cd1c0ab6b2e5e8458134ff
Jeremy Grubard <jgrubard@gmail.com>
2018-06-05 21:45:01 -0400

    commit

--- store/productionUrl.js
+++ store/productionUrl.js
@@ -1,3 +1,4 @@
-const localHost = 'http://192.168.1.6:3000';
+const localHost = 'http://localhost:3000';
+// const localHost = 'http://192.168.1.6:3000';
 const url = localHost || 'http://immense-escarpment-58025.herokuapp.com';
 export default url;
