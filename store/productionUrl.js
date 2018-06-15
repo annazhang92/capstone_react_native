7a2d06849bbf5adeff40195e7dee4941d204303e
Gabriel Rumbaut <grumbaut@gmail.com>
2018-06-14 21:34:35 -0400

    commit

--- store/productionUrl.js
+++ store/productionUrl.js
@@ -1,10 +1,5 @@
 let localHost;
-<<<<<<< HEAD
-// localHost = 'http://172.16.22.76:3000';
-localHost = 'http://192.168.1.172:3000';
-=======
 localHost = 'http://10.13.78.114:3000';
->>>>>>> 8940043247315c95cb5e93a3210ec93c5165cc16
 // localHost = 'http://localhost:3000';
 const url = localHost || 'http://immense-escarpment-58025.herokuapp.com';
 export default url;
